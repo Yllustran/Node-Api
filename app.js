f19d9ccba05cf59a436eedfbd1ba9b264c63ed8d
Yllustran <sullivantra@gmail.com>
2024-07-03 16:13:41 +0200

    - ajout des routes - ajout d'une condition sur les routes : id pokemon 151 maximum

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const port = 3000;
 // app.METHODE(CHEMIN,GESTIONNAIRE(req,res))
 app.get('/', (req, res) => res.send('hello world !'));
 
-app.get('/api/pokemons/1', (req, res) => res.send('Hello, Bulbizarre !'));
+app.get('/api/pokemons/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (id > 151) {
+        res.send('Navré, le pokedex ne contient que 151 pokémons...');
+    } else {
+        res.send(`Vous avez demandé le pokémon n°${id}`);
+    }
+});
 
 app.listen(port, () => console.log(`Application Node est démarré sur : http://localhost:${port}`))
