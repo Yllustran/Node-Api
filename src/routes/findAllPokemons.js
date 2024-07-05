d541f04e776763fac6c8aa6464dfcd5cdd3ee3fc
Yllustran <sullivantra@gmail.com>
2024-07-05 14:24:15 +0200

    - Ajout du middleware Auth sur l'endpoint /api/pokemons (GET) - Correction de l'arborescence 'auth'

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,9 +1,10 @@
 // Importation du modèle Pokemon depuis le fichier sequelize dans le répertoire db
-const { Pokemon } = require('../db/sequelize') 
+const { Pokemon } = require('../db/sequelize');
+const auth = require('../auth/auth');
 module.exports = (app) => {
 
-  // Définition d'une route GET sur l'endpoint /api/pokemons
-  app.get('/api/pokemons', (req, res) => {
+  // Définition d'une route GET sur l'endpoint /api/pokemons + add auth
+  app.get('/api/pokemons', auth, (req, res) => {
 // Utilisation de la méthode findAll du modèle Pokemon pour récupérer tous les enregistrements de pokémons
 Pokemon.findAll()   
     Pokemon.findAll()
